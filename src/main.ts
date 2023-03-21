152fa5f7dc2aa231d246996edb39d2d6b49737bb
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-21 18:47:44 +0900

    readme + 배포

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import './app.css'
-import App from './App.svelte'
+import { initializeApp } from "firebase/app";
+import "./app.css";
+import App from "./App.svelte";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_KEY,
+  authDomain: "dev-jeans.firebaseapp.com",
+  projectId: "dev-jeans",
+  storageBucket: "dev-jeans.appspot.com",
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID,
+};
+
+const firebase = initializeApp(firebaseConfig);
 
 const app = new App({
-  target: document.getElementById('app'),
-})
+  target: document.getElementById("app"),
+});
 
-export default app
+export default app;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,13 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_FIREBASE_KEY: string;
+  readonly VITE_APP_MESSAGING_SENDER_ID: string;
+  readonly VITE_APP_APP_ID: string;
+  readonly VITE_APP_MEASUREMENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
