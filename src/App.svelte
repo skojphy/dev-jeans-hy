59f45ab834e0e031b1b343d258786961aa9dc044
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-22 19:58:14 +0900

    프리티어 적용

--- src/App.svelte
+++ src/App.svelte
@@ -1,81 +1,83 @@
 <script lang="ts">
   import devJeans from './assets/dev-jeans.png'
-  import ColorPicker from 'svelte-awesome-color-picker';
-  import {fabric} from "fabric";
-  import { onMount } from 'svelte';
+  import ColorPicker from 'svelte-awesome-color-picker'
+  import {fabric} from 'fabric'
+  import {onMount} from 'svelte'
 
-  let canvas;
-  let inputImage: HTMLInputElement;
-  let hex = '#F9BB01';
-  let width = 600;
+  let canvas
+  let inputImage: HTMLInputElement
+  let hex = '#F9BB01'
+  let width = 600
 
   const getWidth = () => {
-    if(window.innerWidth < 600) return window.innerWidth;
-    return 600;
+    if (window.innerWidth < 600) return window.innerWidth
+    return 600
   }
 
-  onMount(()=>{
-    canvas = new fabric.Canvas('canvas');
-    width = getWidth();
+  onMount(() => {
+    canvas = new fabric.Canvas('canvas')
+    width = getWidth()
 
-    canvas.setWidth(width * canvas.getZoom());
-    canvas.setHeight(width * canvas.getZoom());
+    canvas.setWidth(width * canvas.getZoom())
+    canvas.setHeight(width * canvas.getZoom())
 
-    fabric.Image.fromURL(devJeans, function(img) {
-      img.scaleToWidth(width);
-      img.scaleToWidth(width);
-      img.selectable = false;
+    fabric.Image.fromURL(devJeans, function (img) {
+      img.scaleToWidth(width)
+      img.scaleToWidth(width)
+      img.selectable = false
       canvas.add(img)
       canvas.renderAll()
-    });
+    })
   })
 
-  $: if(canvas){
-      canvas.setBackgroundColor(hex)
-      canvas.renderAll()
+  $: if (canvas) {
+    canvas.setBackgroundColor(hex)
+    canvas.renderAll()
   }
 
-  const handleAddImage = (e: Event & {
-    currentTarget: EventTarget & HTMLInputElement;
-}) => {
-    removeImage();
-    const files = e.currentTarget.files;
-    if (!files) return;
-    const file = files[0];
-    const url = URL.createObjectURL(file);
+  const handleAddImage = (
+    e: Event & {
+      currentTarget: EventTarget & HTMLInputElement
+    },
+  ) => {
+    removeImage()
+    const files = e.currentTarget.files
+    if (!files) return
+    const file = files[0]
+    const url = URL.createObjectURL(file)
 
     fabric.Image.fromURL(url, function (img) {
-      const left = 235 / 600;
-      const top = 360 / 600;
+      const left = 235 / 600
+      const top = 360 / 600
 
       img.set({
-          left: left * width,
-          top: top * width,
-          angle: -10,
-      });
-      img.scaleToWidth(width / 6);
-      canvas.add(img);
-    });
+        left: left * width,
+        top: top * width,
+        angle: -10,
+      })
+      img.scaleToWidth(width / 6)
+      canvas.add(img)
+    })
   }
 
-  const removeImage = ()=> {
-    const objects = canvas.getObjects();
-    if(objects.length === 1) return;
-    const lastObject = objects[objects.length - 1];
-    canvas.remove(lastObject);
+  const removeImage = () => {
+    const objects = canvas.getObjects()
+    if (objects.length === 1) return
+    const lastObject = objects[objects.length - 1]
+    canvas.remove(lastObject)
   }
 </script>
 
 <main style={`width: ${width}`}>
   <h1>~개발진스짤 만들어 쓰기~</h1>
-  <canvas id="canvas" width="2400" height="2400" style="border:1px solid #ccc"></canvas>
-  
+  <canvas id="canvas" width="2400" height="2400" style="border:1px solid #ccc" />
+
   <div class="toolbar">
     <div>
       <h2>배경색</h2>
-      <ColorPicker bind:hex isA11yClosable={false} label='배경색'/>
+      <ColorPicker bind:hex isA11yClosable={false} label="배경색" />
     </div>
-  
+
     <div>
       <h2>사진 추가</h2>
 
@@ -88,13 +90,15 @@
 
     <div>
       <h2>내보내기</h2>
-      <button on:click={(e) => {
-        const data = canvas.toDataURL({format: 'png', quality: 1, multiplier: 4});
-        const link = document.createElement('a');
-        link.download = 'dev-jeans.png';
-        link.href = data;
-        link.click();
-      }}>저장</button>
+      <button
+        on:click={(e) => {
+          const data = canvas.toDataURL({format: 'png', quality: 1, multiplier: 4})
+          const link = document.createElement('a')
+          link.download = 'dev-jeans.png'
+          link.href = data
+          link.click()
+        }}>저장</button
+      >
     </div>
   </div>
 </main>
@@ -106,31 +110,31 @@
   <span>
     Blog: <a href="https://velog.io/@heelieben">@pigme</a>
   </span>
-
 </footer>
 
 <style>
-  h1{
+  h1 {
     margin: 20px 0;
   }
   a {
     color: #0098fa;
     text-decoration: none;
   }
-  main, footer{
+  main,
+  footer {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     color: rgb(56, 56, 56);
   }
-  footer{
+  footer {
     flex-direction: row;
     gap: 10px;
     font-weight: 800;
-    color: rgb(98, 98, 98)
+    color: rgb(98, 98, 98);
   }
- .toolbar {
+  .toolbar {
     display: flex;
     justify-content: center;
     width: 100%;
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
-import { initializeApp } from "firebase/app";
-import "./app.css";
-import App from "./App.svelte";
+import {initializeApp} from 'firebase/app'
+import './app.css'
+import App from './App.svelte'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_KEY,
-  authDomain: "dev-jeans-14b65.firebaseapp.com",
-  projectId: "dev-jeans-14b65",
-  storageBucket: "dev-jeans-14b65.appspot.com",
+  authDomain: 'dev-jeans-14b65.firebaseapp.com',
+  projectId: 'dev-jeans-14b65',
+  storageBucket: 'dev-jeans-14b65.appspot.com',
   messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_APP_ID,
   measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID,
-};
+}
 
-const firebase = initializeApp(firebaseConfig);
+const firebase = initializeApp(firebaseConfig)
 
 const app = new App({
-  target: document.getElementById("app"),
-});
+  target: document.getElementById('app'),
+})
 
-export default app;
+export default app
