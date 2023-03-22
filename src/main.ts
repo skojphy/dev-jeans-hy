4ab3c509f6a331dc7fa9e7a5f75b1e8d3d7dee5e
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-22 10:58:11 +0900

    파이어베이스 제거

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,5 @@
-import { initializeApp } from "firebase/app";
 import "./app.css";
 import App from "./App.svelte";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_KEY,
-  authDomain: "dev-jeans-14b65.firebaseapp.com",
-  projectId: "dev-jeans-14b65",
-  storageBucket: "dev-jeans-14b65.appspot.com",
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_APP_ID,
-  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID,
-};
-
-const firebase = initializeApp(firebaseConfig);
 
 const app = new App({
   target: document.getElementById("app"),
