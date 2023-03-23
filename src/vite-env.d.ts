1385c0e963a110e5726b023b23cfcb5e67251326
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-23 16:02:53 +0900

    firebase 환경변수 타입 설정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,12 +2,15 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_FIREBASE_KEY: string;
-  readonly VITE_APP_MESSAGING_SENDER_ID: string;
-  readonly VITE_APP_APP_ID: string;
-  readonly VITE_APP_MEASUREMENT_ID: string;
+  readonly VITE_APP_FIREBASE_KEY: string
+  readonly VITE_APP_AUTH_DOMAIN: string
+  readonly VITE_APP_PROJECT_ID: string
+  readonly VITE_APP_STORAGE_BUCKET: string
+  readonly VITE_APP_MESSAGING_SENDER_ID: string
+  readonly VITE_APP_APP_ID: string
+  readonly VITE_APP_MEASUREMENT_ID: string
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv
 }
