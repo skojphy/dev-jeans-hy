b88eb41769b5b7eb53d14045dfdb66425c266ff0
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-23 15:35:23 +0900

    :rocket: base 변경

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import {svelte} from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dev-jeans/',
+  // base: '/dev-jeans/',
   plugins: [svelte()],
 })
