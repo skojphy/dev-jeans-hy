e03bedcb760d4f8d6f72e70c612714d6a1c37138
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-25 14:25:13 +0900

    axios credential 추가

--- src/api/service/auth.ts
+++ src/api/service/auth.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 export const postLogin = async ({idToken}: {idToken: string}) => {
   const result = await axios.post(`${import.meta.env.VITE_APP_API_URL}/v1/oauth/login`, {
     idToken,
+    withCredentials: true,
   })
   return result.data
 }
--- src/api/service/user.ts
+++ src/api/service/user.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 export const getUserInfo = async () => {
-  const result = await axios.get(`${import.meta.env.VITE_APP_API_URL}/user/info`)
+  const result = await axios.get(`${import.meta.env.VITE_APP_API_URL}/user/info`, {
+    withCredentials: true,
+  })
   return result.data
 }
