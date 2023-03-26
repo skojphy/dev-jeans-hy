95cc3d1bda5ddb1db71973e5bc92ccf14cb4fd93
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-26 22:47:02 +0900

    :sparkles: 토큰 만료 케이스 핸들링

--- src/lib/initUser.ts
+++ src/lib/initUser.ts
@@ -9,7 +9,10 @@ export const initUser = async () => {
   const result = await postLogin({idToken: get(idToken)})
 
   // 토큰이 만료되어 로그인 실패
-  if (!result) return
+  if (!result) {
+    idToken.set(null)
+    return
+  }
 
   // 토큰이 유효하면 유저 정보를 가져옴
   const userInfo = await getUserInfo()
