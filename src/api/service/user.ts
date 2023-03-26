57dd5ffed42fed70000365a446084cd883bba7fc
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-26 23:16:02 +0900

    :sparkles: 토큰 만료시 상태 초기화

--- src/api/service/user.ts
+++ src/api/service/user.ts
@@ -1,13 +1,22 @@
 import axios from 'axios'
+import {idToken, userInfo} from 'src/store/user'
 import type {ApiResponseType} from 'src/types/api'
 import type {PhotoRes} from 'src/types/photo'
 import type {UserInfo} from 'src/types/user'
 
 export const getUserInfo = async () => {
-  const result = await axios.get<ApiResponseType<UserInfo>>(`${import.meta.env.VITE_APP_API_URL}/user/info`, {
-    withCredentials: true,
-  })
-  return result.data.data
+  let result = null
+  try {
+    result = await axios.get<ApiResponseType<UserInfo>>(`${import.meta.env.VITE_APP_API_URL}/user/info`, {
+      withCredentials: true,
+    })
+  } catch (e) {
+    if (e.response.status === 403) {
+      idToken.set(null)
+      userInfo.set(null)
+    }
+  }
+  return result?.data?.data
 }
 
 export const getUserPhotos = async () => {
