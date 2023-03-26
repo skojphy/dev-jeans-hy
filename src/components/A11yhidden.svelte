8578227caffafbf3eb1af49f614da386ff6ae191
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-26 14:04:34 +0900

    :sparkles: a11yhidden 컴포넌트 생성

---
+++ src/components/A11yhidden.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let as = 'span'
+</script>
+
+<svelte:element this={as} class="a11yHidden">
+  <slot />
+</svelte:element>
+
+<style>
+  .a11yHidden {
+    overflow: hidden;
+    position: absolute;
+    clip: rect(0, 0, 0, 0);
+    clip-path: circle(0);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    white-space: nowrap;
+  }
+</style>
