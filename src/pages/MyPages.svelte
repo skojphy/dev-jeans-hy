27dcaf386ce70333d9c98c8699faeead9e7f7b1f
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-26 23:04:12 +0900

    :speech_balloon: TODO

--- src/pages/MyPages.svelte
+++ src/pages/MyPages.svelte
@@ -34,6 +34,8 @@
       {#if !$myPhotos.length}
         <div class="no-bunny">
           <h2>나의 버니가 없어요!</h2>
+
+          <!-- TODO. 버니가 6개가 넘어가면 경고 토스트 팝업 노출 -->
           <span>나만의 버니를 업로드해보세요.</span>
 
           <img src={devJeans} alt="버니" class="no-bunny-img" />
