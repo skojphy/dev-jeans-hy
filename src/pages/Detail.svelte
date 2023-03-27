275447d61e39c0e6b41f6103bbae5f04ff59c65f
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-27 22:21:38 +0900

    :sparkles: 디테일 페이지 api 연결

--- src/pages/Detail.svelte
+++ src/pages/Detail.svelte
@@ -2,63 +2,90 @@
   import Layout from 'src/components/Layout/Layout.svelte'
   import likeDefault from 'src/assets/icons/like_default.svg'
   import likeFilled from 'src/assets/icons/like_filled.svg'
-  import deleteIcon from 'src/assets/icons/delete.svg'
-  import A11yhidden from 'src/components/A11yhidden.svelte'
+  import {checkLikePhoto, deletePhoto, getPhoto, toggleLikePhoto} from 'src/api/service/photo'
+  import {params, pop} from 'svelte-spa-router'
+  import {useQueryClient, createQuery, createMutation} from '@tanstack/svelte-query'
+  import {userInfo} from 'src/store/user'
+  import devJeans from 'src/assets/dev-jeans.png'
 
-  // export let params = {
-  //   id: 0,
-  // }
-  // TODO: param.id로 photoInfo 가져오기
+  $: photoQuery = createQuery({
+    queryKey: ['photo', `${$params?.id}`],
+    queryFn: () => getPhoto({id: $params?.id}),
+    enabled: !!$params?.id,
+  })
 
-  const photoInfo = {
-    photoId: 1,
-    photoTitle: '내가 만든 버니',
-    thumbnailImageUrl: 'src/assets/dev-jeans2.png',
-    imageUrl: 'src/assets/dev-jeans2.png',
-    createdDate: '2021-08-01',
-    lastModifiedDate: '2021-08-01',
-    likeCount: 15003,
-    visitCount: 0,
-    creator: '@hyeyeon',
+  // TODO. 좋아요 낙관적 UI 적용
+  $: likeQuery = createQuery({
+    queryKey: ['photo', `${$params?.id}`, 'like'],
+    queryFn: () => checkLikePhoto({photoId: $params?.id}),
+    enabled: !!$params?.id,
+  })
+
+  $: isAuthor = $photoQuery?.data?.userDto.email === $userInfo?.email
+  $: isLiked = $likeQuery?.data
+  $: console.log({isLiked})
+
+  $: creator = $photoQuery?.data?.userDto.email.split('@')[0]
+
+  const client = useQueryClient()
+
+  // TODO. 삭제 되었습니다 토스트 띄우기
+  const deleteMutation = createMutation(() => deletePhoto({id: $photoQuery?.data?.photoId}), {
+    onSuccess: () => {
+      client.invalidateQueries(['bunny-list', 'ranked'])
+      client.invalidateQueries(['bunny-list', 'latest'])
+      client.invalidateQueries(['myPhotos'])
+      pop()
+    },
+  })
+
+  const handleDeletePhoto = async () => {
+    $deleteMutation.mutate()
   }
 
-  const {photoTitle, imageUrl, createdDate, likeCount, creator} = photoInfo
-
-  const isAuthor = true
-  const isLiked = true
-
-  // TODO: 삭제 기능 연결
-  // 작성자인지 user와 비교하기
-  // TODO: 좋아요 기능 연결
+  const handleToggleLikePhoto = async () => {
+    try {
+      isLiked = !isLiked
+      await toggleLikePhoto({isLiked, id: $params?.id})
+      await $photoQuery.refetch()
+    } catch (e) {
+      isLiked = !isLiked
+    }
+  }
 </script>
 
 <Layout title="사진 보기">
   <div class="container">
     <div class="titleArea">
-      <h2 class="title">{photoTitle}</h2>
-      <p class="creator">{creator}</p>
+      <h2 class="title">{$photoQuery?.data?.photoTitle}</h2>
+      <p class="creator">{`@${creator}`}</p>
 
       {#if isAuthor}
-        <button class="delete"> 삭제 </button>
+        <button class="delete" type="button" on:click={handleDeletePhoto}>삭제</button>
       {/if}
     </div>
 
-    <img src={imageUrl} alt="버니" />
+    <img
+      class={$photoQuery?.data?.imageUrl ? '' : 'default'}
+      src={$photoQuery?.data?.imageUrl || devJeans}
+      alt="버니"
+    />
 
     <div class="description">
       <div class="like">
-        <button class="likeButton">
+        <button class="likeButton" on:click={handleToggleLikePhoto}>
           <img src={isLiked ? likeFilled : likeDefault} alt={`좋아요 ${isLiked ? '눌림' : '눌리지 않음'}`} />
         </button>
-        <p class="likes">{likeCount}</p>
+        <p class="likes">{$photoQuery?.data?.likeCount}</p>
       </div>
-      <p class="created">{createdDate}</p>
+      <p class="created">{$photoQuery?.data?.createdDate}</p>
     </div>
   </div>
 </Layout>
 
 <style>
   .container {
+    width: 100%;
     background-color: #fff;
     border: 1px solid #dee2e6;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.04);
@@ -70,6 +97,12 @@
     border-radius: 10px 10px 0 0;
     position: relative;
   }
+  .default {
+    width: 100%;
+    background-color: #dee2e6;
+    opacity: 0.5;
+    filter: grayscale(100%);
+  }
   .title,
   .creator {
     display: inline-block;
@@ -77,7 +110,6 @@
   .description {
     padding: 10px;
     color: #495057;
-    /* border-radius: 0 0 10px 10px; */
     display: flex;
     justify-content: space-between;
     align-items: center;
