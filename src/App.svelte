4de23c71047240f7840f133063d0c249c2845397
cherry.k <cherry.k@kakaoenterprise.com>
2023-03-27 23:50:54 +0900

    :sparkles: 토스트 추가

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Router from 'svelte-spa-router'
   import {QueryClient, QueryClientProvider} from '@tanstack/svelte-query'
-
+  import {SvelteToast} from '@zerodevx/svelte-toast'
   import {routes} from 'src/pages/routes'
   import {onMount} from 'svelte'
   import {initUser} from './lib/initUser'
@@ -11,5 +11,18 @@
 </script>
 
 <QueryClientProvider client={queryClient}>
+  <SvelteToast
+    options={{
+      reversed: true,
+      intro: {y: 192},
+      duration: 1500,
+      dismissable: false,
+      theme: {
+        '--toastBarHeight': 0,
+        '--toastBackground': '#1982c4aa',
+        '--toastBarBackground': '#1982c4',
+      },
+    }}
+  />
   <Router {routes} />
 </QueryClientProvider>
